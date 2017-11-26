0a2db7bf586295ce65e353400cf5b04973261d52
NOEL MAURICE <noelmaurice4@gmail.com>
2017-11-26 18:46:56 +0100

    First commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     * 
+     * http://localhost:8080/ms2i/web/app_dev.php/
+     * 
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
+    
+}
