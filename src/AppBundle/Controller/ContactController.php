0d00966e3d861b9a1ee0bc8fedfb743bffadafcb
NOEL MAURICE <noelmaurice4@gmail.com>
2017-11-27 12:23:58 +0100

    New template is implemented

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/contact")
+ */
+class ContactController extends Controller
+{
+    /**
+     * @Route("/", name="contact")
+     * 
+     * http://localhost:8080/ms2i/web/app_dev.php/contact/
+     *
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('AppBundle:Contact:create.html.twig');
+    }
+    
+}
